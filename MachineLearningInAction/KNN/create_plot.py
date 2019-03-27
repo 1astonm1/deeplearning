ef1b37aa2c015378e9e1a93cd7ef1be1bc99dc65
astonm <45306215+1astonm1@users.noreply.github.com>
2019-03-27 11:33:25 +0800

    2019.3.27.11.33

---
+++ MachineLearningInAction/KNN/create_plot.py
@@ -0,0 +1,13 @@
+from numpy import *
+from MachineLearningInAction.KNN.knn import knnBase
+import matplotlib
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+datingDataMat, datingLabels = knnBase.file2matrix('datingTestSet2.txt')
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+ax.axis([-2, 25, -0.2, 2.0])
+plt.xlabel('Percentage of Time Spent Playing Video Games')
+plt.ylabel('Liters of Ice Cream Consumed Per Week')
+plt.show()
