6266d71104d71805333b3f944141bd2a8de8016d
astonm <45306215+1astonm1@users.noreply.github.com>
2019-04-01 21:01:00 +0800

    2019.4.1.21.00

--- DeepLearning/TensorflowLearning/base/first_nerual_network/first_nerual_network.py
+++ DeepLearning/TensorflowLearning/base/first_nerual_network/first_nerual_network.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 神经层里常见的参数通常有weights、biases和激励函数。
 
@@ -24,12 +25,19 @@ y_data = np.square(x_data) - 0.5 + noise
 # 利用占位符定义我们所需的神经网络的输入 None表示给多少个sample都可以 1表示有一个特征
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
+# 显示数据集
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()  # 本次运行请注释，全局运行不要注释
+plt.show()
+
 
 # 我们开始定义隐藏层,利用之前的add_layer()函数
 
 
 def creat_nerual_network():
-    l1 = add_layer(xs, 1, 10, activation_function=tf.nn.sigmoid)
+    l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
     # 定义输出层。此时的输入就是隐藏层的输出——l1，输入有10层（隐藏层的输出层），输出有1层。
     prediction = add_layer(l1, 10, 1, activation_function=None)
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
@@ -37,18 +45,31 @@ def creat_nerual_network():
     train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
     # 梯度下降优化训练 0.1代表最小误差
 
-    return train_step, loss
+    return train_step, loss, prediction
 
 
 def train():
-    train_step, loss, init = creat_nerual_network()
+    train_step, loss, prediction = creat_nerual_network()
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
-    for i in range(10000):
+
+    for i in range(1500):
         sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
         if i % 50 == 0:
             print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+            try:
+                ax.lines.remove(lines[0])
+            except Exception:
+                pass
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            ax.scatter(x_data, y_data)
+            prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+            # plot the prediction
+            lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+            plt.pause(0.1)
+            plt.show()
     sess.close()
 
 
